7715bf399fba649b1c3242be7871a1a10dcee1de
Taylor Shuff <tks2n@mtmail.mtsu.edu>
2017-03-15 10:41:56 -0500

    added DAL folder with IRepository interface with create, read, update, and delete methods.

---
+++ FakeBlog/DAL/IRepository.cs
@@ -0,0 +1,27 @@
+﻿using FakeBlog.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FakeBlog.DAL
+{
+    interface IRepository
+    {
+        //List of methods to help deliver features
+        //Create
+        void AddPost(Post post);
+
+        //Read
+        Post GetPost(int postId);
+        List<Post> GetPostFromAuthor(int authorId);
+
+        //Update
+        bool Publish(int postId);
+        bool Edit(int postId, string body);
+
+        //Delete
+        bool RemovePost(int postId);
+    }
+}
