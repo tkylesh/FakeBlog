2392693db4dcb496006a97e95a7a7f01e134486f
Taylor Shuff <tks2n@mtmail.mtsu.edu>
2017-03-15 17:42:59 -0500

    added migrations and repository that implements IRepository.

---
+++ FakeBlog/DAL/FakeBlogRepository.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using FakeBlog.Models;
+
+namespace FakeBlog.DAL
+{
+    public class FakeBlogRepository : IRepository
+    {
+        public void AddPost(string Title, Author author, string body, bool IsDraft)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Edit(int postId, string body)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Post GetPost(int postId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Post> GetPostFromAuthor(int authorId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Publish(int postId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool RemovePost(int postId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
